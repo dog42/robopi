0f978ef5f4dbc352b2acec4bf232173061499d03
dockhorn <dockhorn@innoroute.de>
2017-05-31 21:24:06 +0200

    startcam.sh variable ip and port

--- startcam.sh
+++ startcam.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+ip=192.168.1.211
+port=5000
+if  [[ $# == 1 ]]
+	then
+	ip=$1
+elif [[ $# == 2 ]]
+	then
+        ip=$1
+	port=$2
+fi
+
+echo Verbinde zu: $ip:$port
+
 gst-launch-1.0 rpicamsrc bitrate=1000000 \
     ! 'video/x-h264,width=640,height=480' \
     ! h264parse \
     ! queue \
     ! rtph264pay config-interval=1 pt=96 \
     ! gdppay \
-    ! udpsink host=[INSERT_IP_ADDR] port=5000
+    ! udpsink host=$ip port=$port
