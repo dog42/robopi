faccd3cfa89c5344d03af8a1ce733d08f5eb327b
D0gi <s122106@hft-leipzig.de>
2017-06-07 19:39:19 +0200

    better control_start script

--- robo_ctrl/start.sh
+++ robo_ctrl/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
+piip=robopi
+myip=192.168.1.211
+port=5005
 ./cam.sh &
-robo_control
+ssh pi@$piip "cd /home/pi/robopi/; ./autostart.sh $myip; exit;" &
+sleep 2
+echo '>>>> WAIT...'
+sleep 3
+echo '>>>> WAIT...'
+sleep 3
+echo '>>>> START...'
+sudo ./robo_control $piip $port /dev/hidraw3 1
 
+killall gst-launch-1.0
+killall robo
 
